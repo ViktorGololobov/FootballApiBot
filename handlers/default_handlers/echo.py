23a5b04683e2dbcc2cc75fae18ab734139b81b3b
Viktor Gololobov <vyg1996@gmail.com>
2024-10-09 13:04:47 +0400

    добавил проверку на приветствие бота человеком и ответа на него

--- handlers/default_handlers/echo.py
+++ handlers/default_handlers/echo.py
@@ -2,8 +2,28 @@ from telebot.types import Message
 from loader import bot
 
 
+greetings_list = [
+    'привет', 'добрый день', 'добрый вечер', 'доброе утро', 'здравствуйте', 'здравствуй', 'приветствую', 'здрасьте',
+    'доброго дня', 'доброго вечера', 'доброго утра'
+]
+
+
 @bot.message_handler(state=None)
 def bot_echo(message: Message):
     """ Эхо хендлер, куда летят текстовые сообщения без указанного состояния. """
-    bot.reply_to(message, f"Я вас не понимаю. \n"
-                          f"Для того, чтобы начать, введите команду /start или, если вам нужна помощь, введите /help.")
+
+    global greetings_list
+
+    for greeting in greetings_list:
+        if greeting in message.text.lower():
+            bot.reply_to(
+                message,
+                'Привет! Для того, чтобы начать, введите команду /start или, если вам нужна помощь, введите /help.'
+            )
+            break
+    else:
+        bot.reply_to(
+            message,
+            "Я вас не понимаю. \n"
+            "Для того, чтобы начать, введите команду /start или, если вам нужна помощь, введите /help."
+        )
